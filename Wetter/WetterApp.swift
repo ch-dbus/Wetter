1f79b959f79b3f8118f5b0c70f56d734680ec690
Dominik Bussinger <d.bussinger@gmail.com>
2024-07-15 10:32:41 +0200

    Initial Commit

---
+++ Wetter/WetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WetterApp.swift
+//  Wetter
+//
+//  Created by Dominik Bussinger on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
